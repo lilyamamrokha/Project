61bd3798324a730a234a6c841fc37a7db2107a19
lilyamamrokha <antonyuk.liliya.andr@gmail.com>
2019-11-17 18:25:14 +0000

    Added css + js

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+
+document.querySelector('.text-form .submit-botton')
+    .addEventListener('click', submitForm);
+
+function login(e) {
+    e.preventDefault();
+    
+}
+function submitForm(e) {
+    e.preventDefault();
+    fetch('makeorder', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: document.querySelector('.text-form input[name=userName]').value,
+            email: document.querySelector('.text-form input[name=userMail]').value,
+            message: document.querySelector('.text-form textarea[name=userMessage]').value
+        })
+    })
+        .then(_ => document.querySelector('.text-form').reset());
+}
